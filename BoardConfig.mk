98dd4507d39e0f25173ed7f1ea3f1064ec927d66
translucentfocus <xchamster@gmail.com>
2018-12-21 05:19:27 -0500

    Initial Board Config
    Needs lots of updating... stole most of it from Dev Tree for cheryl (Razer Phone 1)

---
+++ BoardConfig.mk
@@ -0,0 +1,101 @@
+DEVICE_TREE := device/razer/chery2
+
+# Bootloader
+TARGET_NO_BOOTLOADER := true
+# TODO get proper board name
+# TARGET_BOOTLOADER_BOARD_NAME := msm8998
+
+# Platform
+#TODO update to correct info below
+# TARGET_BOARD_PLATFORM := msm8998
+# TARGET_BOARD_PLATFORM_GPU := qcom-adreno540
+
+# Flags
+#TARGET_GLOBAL_CFLAGS +=
+#TARGET_GLOBAL_CPPFLAGS +=
+#COMMON_GLOBAL_CFLAGS +=
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+#TODO verify kyro is correct
+TARGET_CPU_VARIANT := kryo
+TARGET_CPU_SMP := true
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53
+
+# Kernel
+#TARGET_KERNEL_SOURCE := kernel/razer/msm8998
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_KERNEL_CONFIG := twrp_cheryl2_defconfig
+
+TARGET_PREBUILT_KERNEL := $(DEVICE_TREE)/Image.gz-dtb
+
+# Boot image
+#TODO verify/fix all of this
+#This is the board info from cheryl (1st generation)
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 androidboot.hardware=qcom androidboot.selinux=permissive
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x02000000 --tags_offset 0x00000100
+
+# Partitions
+# TODO fix partition addresses below
+# BOARD_BOOTIMAGE_PARTITION_SIZE     := 0x0002800000
+# BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x0002880000
+# BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 0x018FA00000
+# BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x1BC4E74000
+# BOARD_CACHEIMAGE_PARTITION_SIZE    := 0x0020000000
+# BOARD_FLASH_BLOCK_SIZE := 0x40000
+
+# File systems
+BOARD_HAS_LARGE_FILESYSTEM := true
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+
+# A/B device flags
+#TARGET_NO_RECOVERY := true
+#BOARD_USES_RECOVERY_AS_BOOT := true
+#BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
+AB_OTA_UPDATER := true
+
+# TWRP specific build flags
+TW_THEME := portrait_hdpi
+RECOVERY_SDCARD_ON_DATA := true
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/config/usb_gadget/g1/functions/mass_storage.0/lun.%d/file"
+TW_BRIGHTNESS_PATH := "/sys/devices/soc/c900000.qcom\x2cmdss_mdp/c900000.qcom\x2cmdss_mdp:qcom\x2cmdss_fb_primary/leds/lcd-backlight/brightness"
+TW_MAX_BRIGHTNESS := 255
+TW_DEFAULT_BRIGHTNESS := 158
+TW_SCREEN_BLANK_ON_BOOT := true
+TW_INCLUDE_NTFS_3G := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
+BOARD_SUPPRESS_SECURE_ERASE := true
+# Stock kernel sometimes crashes when we toggle MTP
+TW_EXCLUDE_MTP := true
+
+# We can use the factory reset button combo to enter recovery safely
+TW_IGNORE_MISC_WIPE_DATA := true
+
+# exFAT drivers included in the kernel
+TW_NO_EXFAT_FUSE := true
+
+# Encryption support
+TW_INCLUDE_CRYPTO := true
+TARGET_HW_DISK_ENCRYPTION := true
+#TARGET_KEYMASTER_WAIT_FOR_QSEE := true
+
+# Asian region languages
+TW_EXTRA_LANGUAGES := true
+
+# Debug flags
+#TWRP_INCLUDE_LOGCAT := true
+TARGET_USES_LOGD := true

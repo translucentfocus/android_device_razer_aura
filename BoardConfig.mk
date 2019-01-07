2a57a4cd2e9a84b7e8c8239fe9bf86d6cffc6b15
translucentfocus <26986716+translucentfocus@users.noreply.github.com>
2019-01-06 23:17:30 -0600

    remove unnecessary cmd
    This is added when running lunch when you pick your build... it shouldn't already be there

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,7 +44,7 @@ TARGET_PREBUILT_KERNEL := $(DEVICE_TREE)/Image.gz-dtb
 # Boot image
 #TODO verify Kernel CMDLine
 # pulled from stock Razer Phone 2 boot.img with Android Kitchen
-BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0xA84000 androidboot.hardware=qcom androidboot.console=ttyMSM0 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 service_locator.enable=1 swiotlb=2048 androidboot.configfs=true androidboot.usbcontroller=a600000.dwc3 buildvariant=user
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0xA84000 androidboot.hardware=qcom androidboot.console=ttyMSM0 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 service_locator.enable=1 swiotlb=2048 androidboot.configfs=true androidboot.usbcontroller=a600000.dwc3
 # Verified below from extracted boot.img using Android Kitchen ramdisk_offset needed to be updated
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096

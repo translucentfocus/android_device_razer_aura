5a3923d4de334e25d95d10c60b3c92ece603d366
translucentfocus <26986716+translucentfocus@users.noreply.github.com>
2019-01-06 23:27:23 -0600

    Leave this crypto stuff out for now

--- device.mk
+++ device.mk
@@ -3,10 +3,10 @@
 
  # Enable update engine sideloading by including the static version of the
 # boot_control HAL and its dependencies.
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.sdm845 \
-    librecovery_updater_msm \
-    libsparse
+# PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+#    bootctrl.sdm845 \
+#    librecovery_updater_msm \
+#    libsparse
 
- PRODUCT_PACKAGES += \
-    update_engine_sideload
+# PRODUCT_PACKAGES += \
+#    update_engine_sideload

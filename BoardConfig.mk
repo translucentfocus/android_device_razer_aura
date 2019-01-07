cea5f87a81b1919ce6353f2655e43d2379f44c7c
translucentfocus <26986716+translucentfocus@users.noreply.github.com>
2019-01-06 23:24:04 -0600

    Removes compiler warning
    Compiler warns that neon treated as armv8-a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,10 +28,10 @@ TARGET_CPU_VARIANT := kryo
 TARGET_CPU_SMP := true
 
 TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_ARCH_VARIANT := armv8-a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a53
+TARGET_2ND_CPU_VARIANT := armv8-a
 
 # Kernel
 #   Needed to build from source

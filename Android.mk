b374a704a229085825e6c827bf7a6828393b3078
translucentfocus <26986716+translucentfocus@users.noreply.github.com>
2019-01-06 21:19:41 -0600

    Lol... finally fixed the lunch command

--- Android.mk
+++ Android.mk
@@ -23,10 +23,4 @@
 # *not* include it on all devices, so it is safe even with hardware-specific
 # components.
 
-ifneq ($(filter aura,$(TARGET_DEVICE)),)
-
-LOCAL_PATH := $(call my-dir)
-
-include $(call all-makefiles-under,$(LOCAL_PATH))
-
-endif
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/omni_aura.mk
